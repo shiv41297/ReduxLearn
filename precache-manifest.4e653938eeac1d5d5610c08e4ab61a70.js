ea693b2f6ff83e76d8e42853adf6c2c0eba0ac75
shiv41297 <shivam.singh@appinventiv.com>
2020-04-22 15:59:16 +0530

    Updates

---
+++ precache-manifest.4e653938eeac1d5d5610c08e4ab61a70.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dda67e338e0e0a20ef6aec7c534a240e",
+    "url": "/ReduxLearn/index.html"
+  },
+  {
+    "revision": "bc93b4369fd48ceb5a74",
+    "url": "/ReduxLearn/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8ccca40a77d7df7d28ea",
+    "url": "/ReduxLearn/static/js/2.3b846ee8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReduxLearn/static/js/2.3b846ee8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc93b4369fd48ceb5a74",
+    "url": "/ReduxLearn/static/js/main.846d48ce.chunk.js"
+  },
+  {
+    "revision": "413d0e963e0edbb26bc7",
+    "url": "/ReduxLearn/static/js/runtime-main.dd5d3359.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ReduxLearn/static/media/logo.5d5d9eef.svg"
+  }
+]);
